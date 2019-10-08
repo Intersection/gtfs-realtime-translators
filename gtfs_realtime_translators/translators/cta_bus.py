5b486adb7887cf9ec8e42cd346a0969624622d8d
Jack Hayford <jack.hayford@intersection.com>
2019-10-08 12:16:34 -0400

    Introduce CTA Subway and Bus Translators

---
+++ gtfs_realtime_translators/translators/cta_bus.py
@@ -0,0 +1,45 @@
+import json
+
+import pendulum
+
+from gtfs_realtime_translators.factories import TripUpdate, FeedMessage
+
+
+class CtaBusGtfsRealtimeTranslator:
+    TIMEZONE = 'America/Chicago'
+
+    def __call__(self, data):
+        json_data = json.loads(data)
+        predictions = json_data['bustime-response']['prd']
+        entities = [self.__make_trip_update(idx, arr) for idx, arr in enumerate(predictions)]
+
+        return FeedMessage.create(entities=entities)
+
+
+    @classmethod
+    def __to_unix_time(cls, time):
+        return pendulum.parse(time).in_tz(cls.TIMEZONE).int_timestamp
+
+    @classmethod
+    def __make_trip_update(cls, _id, prediction):
+        entity_id = str(_id + 1)
+        route_id = prediction['rt']
+        stop_id = prediction['stpid']
+
+        # Per the docs, the prediction is either for a scheduled departure, or realtime arrival
+        departure_type = 'D'
+        is_scheduled = prediction['typ'] == departure_type
+
+        parsed_arrival_time = cls.__to_unix_time(prediction['prdtm'])
+        arrival_time = None if is_scheduled else parsed_arrival_time
+
+        ##### Intersection Extensions
+        headsign = prediction['des']
+        scheduled_arrival_time = parsed_arrival_time if is_scheduled else None
+
+        return TripUpdate.create(entity_id=entity_id,
+                                route_id=route_id,
+                                stop_id=stop_id,
+                                arrival_time=arrival_time,
+                                headsign=headsign,
+                                scheduled_arrival_time=scheduled_arrival_time)
---
+++ test/test_cta_bus.py
@@ -0,0 +1,53 @@
+import pytest
+import pendulum
+
+from gtfs_realtime_translators.translators import CtaBusGtfsRealtimeTranslator
+from gtfs_realtime_translators.bindings import intersection_pb2 as intersection_gtfs_realtime
+from gtfs_realtime_translators.factories import FeedMessage
+
+
+@pytest.fixture
+def cta_bus():
+    with open('test/fixtures/cta_bus.json') as f:
+        raw = f.read()
+    return raw
+
+
+def test_cta_bus_realtime_arrival(cta_bus):
+    translator = CtaBusGtfsRealtimeTranslator()
+    with pendulum.test(pendulum.datetime(2019, 2, 20, 17)):
+        message = translator(cta_bus)
+
+    entity = message.entity[0]
+    trip_update = entity.trip_update
+    stop_time_update = trip_update.stop_time_update[0]
+
+    assert message.header.gtfs_realtime_version == FeedMessage.VERSION
+
+    assert entity.id == '1'
+    assert entity.trip_update.trip.route_id == '147'
+    assert stop_time_update.stop_id == '1203'
+    assert stop_time_update.arrival.time == 1570531080
+
+    # Test Intersection extensions
+    intersection_trip_update = trip_update.Extensions[intersection_gtfs_realtime.intersection_trip_update]
+    assert intersection_trip_update.headsign == 'Howard Station'
+
+    intersection_stop_time_update = stop_time_update.Extensions[intersection_gtfs_realtime.intersection_stop_time_update]
+    assert intersection_stop_time_update.scheduled_arrival.time == 0
+
+def test_cta_bus_scheduled_departure(cta_bus):
+    translator = CtaBusGtfsRealtimeTranslator()
+    with pendulum.test(pendulum.datetime(2019, 2, 20, 17)):
+        message = translator(cta_bus)
+
+    entity = message.entity[1]
+    trip_update = entity.trip_update
+    stop_time_update = trip_update.stop_time_update[0]
+
+    assert entity.id == '2'
+    assert stop_time_update.arrival.time == 0
+
+    # Test Intersection extensions
+    intersection_stop_time_update = stop_time_update.Extensions[intersection_gtfs_realtime.intersection_stop_time_update]
+    assert intersection_stop_time_update.scheduled_arrival.time == 1570531500
