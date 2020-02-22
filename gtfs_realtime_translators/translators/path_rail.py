3ba3ba4d8fff12c180bab4bcf1689700bc784812
Dorian Ellerbe <dorian.ellerbe@intersection.com>
2020-02-22 16:17:55 -0500

    Test skeleton

--- gtfs_realtime_translators/translators/path_rail.py
+++ gtfs_realtime_translators/translators/path_rail.py
@@ -1,3 +1,6 @@
+import json
+
+from gtfs_realtime_translators.factories import FeedMessage
 
 
 class PathGtfsRealtimeTranslator:
@@ -14,4 +17,15 @@ class PathGtfsRealtimeTranslator:
         headsign = data[<headsign>]
         custom_status = data[<arrivalTimeMessage>]
     """
-    pass
+
+    def __call__(self, data):
+        json_data = json.loads(data)
+        entities = self.__make_trip_updates(json_data)
+        return FeedMessage.create(entities=entities)
+
+    @classmethod
+    def __make_trip_updates(cls, data):
+        print(data)
+        trip_updates = []
+
+        return trip_updates
--- test/test_path_rail.py
+++ test/test_path_rail.py
@@ -1,5 +1,9 @@
 import pytest
 
+from gtfs_realtime_translators.factories import FeedMessage
+from gtfs_realtime_translators.translators.path_rail import PathGtfsRealtimeTranslator
+from gtfs_realtime_translators.bindings import intersection_pb2 as intersection_gtfs_realtime
+
 
 @pytest.fixture
 def path_rail():
@@ -9,4 +13,20 @@ def path_rail():
 
 
 def test_path_data(path_rail):
-    pass
+    translator = PathGtfsRealtimeTranslator()
+    message = translator(path_rail)
+
+    assert message.header.gtfs_realtime_version == FeedMessage.VERSION
+
+    entity = message.entity[0]
+    trip_update = entity.trip_update
+    assert trip_update.trip.trip_id == '88888'
+    assert trip_update.trip.route_id == '99999'
+
+    stop_time_update = trip_update.stop_time_update[0]
+    assert stop_time_update.stop_id == 'a-stop-id'
+    assert stop_time_update.departure.time == 1570045710
+    assert stop_time_update.arrival.time == 1570045710
+
+    intersection_trip_update = trip_update.Extensions[intersection_gtfs_realtime.intersection_trip_update]
+    assert intersection_trip_update.custom_status == '3 min'
