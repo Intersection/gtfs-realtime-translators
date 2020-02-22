9d0129e6c0f85e83c3557342f31ada08256c6054
Dorian Ellerbe <dorian.ellerbe@intersection.com>
2020-02-22 17:58:07 -0500

    First pass at retrieving a stopId

--- gtfs_realtime_translators/translators/path_rail.py
+++ gtfs_realtime_translators/translators/path_rail.py
@@ -25,7 +25,48 @@ class PathGtfsRealtimeTranslator:
 
     @classmethod
     def __make_trip_updates(cls, data):
-        print(data)
+        route_id_lookup = {'#D93A30': '862',
+                           '#4D92FB,#FF9900': '1024',
+                           '#FF9900': '861'}
+
+        stop_id_lookup = {'862_NWK_NWK': '781719',
+                          '862_NWK_HAR': '781721',
+                          '862_EXP_HAR': '781721',
+                          '862_NWK_JSQ': '781725',
+                          '862_EXP_JSQ': '781725',
+                          '862_NWK_GRV': '781727',
+                          '862_EXP_GRV': '781727',
+                          '862_NWK_EXP': '781731',
+                          '862_EXP_EXP': '781731',
+                          '862_NWK_WTC': '794724',
+                          '862_WTC_NWK': '781718',
+                          '862_WTC_HAR': '781720',
+                          '862_WTC_JSQ': '781722',
+                          '862_WTC_GRV': '781726',
+                          '862_WTC_EXP': '781730',
+                          '862_WTC_WTC': '794724'}
+
+        # linecolor_considered_station_target
+        arrivals = data['results']
+        for arrival in arrivals:
+            arrival_updates = arrival['messages']
+            for update in arrival_updates:
+                try:
+                    route_id = route_id_lookup[update['lineColor']]
+                    # print(route_id)
+                    destination = arrival['target']
+                    # print(destination)
+                    station = arrival['consideredStation']
+                    # print(station)
+
+                    # create the key for the stopId lookup
+                    key = route_id + '_' + destination + '_' + station
+                    # print(key)
+                    stop_id = stop_id_lookup[key]
+                    print(stop_id)
+                except KeyError:
+                    pass
+
         trip_updates = []
 
         return trip_updates
