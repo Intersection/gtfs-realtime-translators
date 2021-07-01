15c6bc48fafe8755a0402cacc2a39203cd7fa2cb
Manan <manan.patel@intersection.com>
2021-07-01 19:03:02 +0530

    TRAN-5069 : Updated NJT BUS Translater with new feed

--- gtfs_realtime_translators/translators/njt_bus.py
+++ gtfs_realtime_translators/translators/njt_bus.py
@@ -1,6 +1,5 @@
 import pendulum
 import xmltodict
-import datetime
 from gtfs_realtime_translators.factories import FeedMessage, TripUpdate
 
 
@@ -8,61 +7,93 @@ class NjtBusGtfsRealtimeTranslator:
 
     TIMEZONE = 'America/New_York'
 
+    def __init__(self, stop_list):
+        self.filtered_stops = stop_list.split(',')
+        if self.filtered_stops is None:
+            raise ValueError('filtered_stops is required.')
+
     def __call__(self, data):
         station_data = xmltodict.parse(data)
-        entities = self.__make_trip_updates(station_data)
+        entities = self.__make_trip_updates(station_data, self.filtered_stops)
         return FeedMessage.create(entities=entities)
 
     @classmethod
     def __to_unix_time(cls, time):
-        dt = pendulum.from_format(
-            time, 'MM/DD/YYYY HH:mm:ss A', tz=cls.TIMEZONE).in_tz('UTC')
+        dt = pendulum.from_format(time, 'DD-MMM-YY HH:mm A', tz=cls.TIMEZONE).in_tz('UTC')
         return dt
 
     @classmethod
-    def __replace_time(cls, sched_dep_time, new_time):
-        dtime = pendulum.from_format(new_time, 'HH:mm:ss', tz=cls.TIMEZONE)
-        hour = dtime.hour
-        minute = dtime.minute
-        second = dtime.second
-        dt = pendulum.from_format(
-            sched_dep_time, 'MM/DD/YYYY HH:mm:ss A', tz=cls.TIMEZONE)
-        dt = dt.replace(hour=hour, minute=minute, second=second)
-        dt = dt.in_tz('UTC')
-        return dt
-
-    @classmethod
-    def __make_trip_updates(cls, data):
+    def __make_trip_updates(cls, data, filtered_stops):
         trip_updates = []
-        trips = data['NEXTTRIPROWSET'].values()
+        trips = data['SCHEDULEROWSET'].values()
 
         for value in trips:
             for idx, item_entry in enumerate(value):
                 # Intersection Extensions
-                headsign = item_entry['header']
-                stop_id = item_entry['stop_id']
-                stop_name = item_entry['stop_name']
-                trip_id = item_entry['Trip_id']
+                headsign = item_entry['busheader']
+                trip_id = item_entry['gtfs_trip_id']
                 route_id = item_entry['route']
-                scheduled_datetime = cls.__to_unix_time(
-                    item_entry['sched_dep_time'])
-                arrival_time = int(cls.__replace_time(
-                    item_entry['sched_dep_time'], item_entry['arrival_time']).timestamp())
-                departure_time = int(cls.__replace_time(
-                    item_entry['sched_dep_time'], item_entry['arrival_time']).timestamp())
-                scheduled_departure_time = int(scheduled_datetime.timestamp())
+                if item_entry.get('STOP', None) is not None:
+                    stops = item_entry['STOP']
 
-                trip_update = TripUpdate.create(entity_id=str(idx + 1),
-                                                route_id=route_id,
-                                                trip_id=trip_id,
-                                                stop_id=stop_id,
-                                                stop_name=stop_name,
-                                                headsign=headsign,
-                                                departure_time=departure_time,
-                                                arrival_time=arrival_time,
-                                                scheduled_departure_time=scheduled_departure_time,
-                                                scheduled_arrival_time=scheduled_departure_time,
-                                                agency_timezone=cls.TIMEZONE)
-                trip_updates.append(trip_update)
+                    # If there is only one <STOP> for the <TRIP> we have to explicitly create a list
+                    if not isinstance(stops, list):
+                       stops = [stops]
+
+                    # Process Stops
+                    for stop in stops:
+                        stop_code = stop['gtfs_stop_Code']
+                        # Get Stop ID for the given Stop Code From the Mapping
+                        stop_id = NJTBusStopCodeIdMappings.get_stop_id(stop_code)
+
+                        # Skip Stop if stop_id is not found
+                        if stop_id is None:
+                            continue
+
+                        # Skip Stop if stop_id is not in filtered stops
+                        if filtered_stops.index(stop_id) < 0 :
+                            continue
+
+                        stop_name = stop['stopname']
+                        track = stop['manual_lane_gate']
+                        if track is None:
+                            track = stop['scheduled_lane_gate']
+
+                        arrival_delay = stop['sec_late']
+
+                        scheduleddeparturedate = stop['scheduleddeparturedate']
+                        scheduleddeparturetime = stop['scheduleddeparturetime']
+                        scheduled_datetime = cls.__to_unix_time("{} {}".format(scheduleddeparturedate.title(), scheduleddeparturetime))
+                        scheduled_departure_time = int(scheduled_datetime.timestamp())
+
+                        trip_update = TripUpdate.create(entity_id=str(idx + 1),
+                                                        route_id=route_id,
+                                                        trip_id=trip_id,
+                                                        stop_id=stop_id,
+                                                        headsign=headsign,
+                                                        stop_name = stop_name,
+                                                        track = track,
+                                                        scheduled_departure_time = scheduled_departure_time,
+                                                        scheduled_arrival_time = scheduled_departure_time,
+                                                        agency_timezone=cls.TIMEZONE)
+                        trip_updates.append(trip_update)
 
         return trip_updates
+
+
+class NJTBusStopCodeIdMappings:
+
+    stops = {
+        '20883':'2916',
+        '19001':'39787',
+        '19161':'39142',
+        '18582':'2183',
+        '21708':'40398',
+    }
+
+    @classmethod
+    def get_stop_id(cls, stop_code):
+        try:
+            return cls.stops[stop_code]
+        except KeyError:
+            return None
--- test/test_njt_bus.py
+++ test/test_njt_bus.py
@@ -13,29 +13,28 @@ def njt_bus():
 
 
 def test_njt_data(njt_bus):
-    translator = NjtBusGtfsRealtimeTranslator()
+    translator = NjtBusGtfsRealtimeTranslator(stop_list = "2916, 39787")
     message = translator(njt_bus)
-
     entity = message.entity[0]
     trip_update = entity.trip_update
+
     stop_time_update = trip_update.stop_time_update[0]
 
     assert message.header.gtfs_realtime_version == FeedMessage.VERSION
     assert entity.id == '1'
-    assert trip_update.trip.route_id == '10'
-    assert trip_update.trip.trip_id == '630'
+    assert trip_update.trip.route_id == '64J'
+    assert trip_update.trip.trip_id == '22899'
 
-    assert stop_time_update.stop_id == '43249'
-    assert stop_time_update.arrival.time == 1621435140
-    assert stop_time_update.departure.time == 1621435140
+    assert stop_time_update.stop_id == '2916'
 
     intersection_trip_update = trip_update.Extensions[
         intersection_gtfs_realtime.intersection_trip_update]
-    assert intersection_trip_update.headsign == '10 JERSEY CITY JOURNAL SQUARE-Exact Fare'
+    assert intersection_trip_update.headsign == 'Weehawken Via Journal Sq'
     assert intersection_trip_update.agency_timezone == 'America/New_York'
 
     intersection_stop_time_update = stop_time_update.Extensions[
         intersection_gtfs_realtime.intersection_stop_time_update]
-    assert intersection_stop_time_update.scheduled_arrival.time == 1621435020
-    assert intersection_stop_time_update.scheduled_departure.time == 1621435020
-    assert intersection_stop_time_update.stop_name == 'JOURNAL SQUARE TRANSPORTATION CENTER'
+    assert intersection_stop_time_update.scheduled_arrival.time == 1625143140
+    assert intersection_stop_time_update.scheduled_departure.time == 1625143140
+    assert intersection_stop_time_update.stop_name == 'Journal Square Transportation Center'
+    assert intersection_stop_time_update.track == 'C-1'
