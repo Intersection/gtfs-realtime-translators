a5e45398af021de29c486e4c465c9c42f5260860
Josh Verma <josh.verma@intersection.com>
2022-03-28 09:11:39 -0400

    TRAN-5926 updated indentations to 4 spaces

--- gtfs_realtime_translators/translators/mbta.py
+++ gtfs_realtime_translators/translators/mbta.py
@@ -14,47 +14,46 @@ class MbtaGtfsRealtimeTranslator:
         entities = self.__make_trip_updates(predictions)
         return FeedMessage.create(entities=entities)
 
-
     @classmethod
     def __to_unix_time(cls, time):
-      return pendulum.parse(time).in_tz(cls.TIMEZONE).int_timestamp
+        return pendulum.parse(time).in_tz(cls.TIMEZONE).int_timestamp
 
     @classmethod
     def __make_trip_updates(cls, predictions):
-      trip_updates = []
-      for idx, prediction in enumerate(predictions):
-        entity_id = str(idx + 1)
-        relationships = prediction['relationships']
-        attributes = prediction['attributes']
-        stop_id = relationships['stop']['data']['id']
-        route_id = relationships['route']['data']['id']
-        trip_id = relationships['trip']['data']['id']
-        raw_arrival_time = attributes['arrival_time']
-        raw_departure_time = attributes['departure_time']
+        trip_updates = []
+        for idx, prediction in enumerate(predictions):
+            entity_id = str(idx + 1)
+            relationships = prediction['relationships']
+            attributes = prediction['attributes']
+            stop_id = relationships['stop']['data']['id']
+            route_id = relationships['route']['data']['id']
+            trip_id = relationships['trip']['data']['id']
+            raw_arrival_time = attributes['arrival_time']
+            raw_departure_time = attributes['departure_time']
 
-        if cls.should_capture_prediction(raw_arrival_time, raw_departure_time):
-          if raw_arrival_time and raw_departure_time:
-            arrival_time = cls.__to_unix_time(raw_arrival_time)
-            departure_time = cls.__to_unix_time(raw_departure_time)
-          elif raw_arrival_time:
-            arrival_time = cls.__to_unix_time(raw_arrival_time)
-            departure_time = arrival_time
-          elif raw_departure_time:
-            departure_time = cls.__to_unix_time(raw_departure_time)
-            arrival_time = departure_time
-                      
-          trip_update = TripUpdate.create(
-            entity_id=entity_id,
-            route_id=route_id,
-            stop_id=stop_id,
-            trip_id=trip_id,
-            arrival_time=arrival_time,
-            departure_time=departure_time
-          )
-          trip_updates.append(trip_update)
-      
-      return trip_updates
+            if cls.should_capture_prediction(raw_arrival_time, raw_departure_time):
+                if raw_arrival_time and raw_departure_time:
+                    arrival_time = cls.__to_unix_time(raw_arrival_time)
+                    departure_time = cls.__to_unix_time(raw_departure_time)
+                elif raw_arrival_time:
+                    arrival_time = cls.__to_unix_time(raw_arrival_time)
+                    departure_time = arrival_time
+                elif raw_departure_time:
+                    departure_time = cls.__to_unix_time(raw_departure_time)
+                    arrival_time = departure_time
+
+                trip_update = TripUpdate.create(
+                    entity_id=entity_id,
+                    route_id=route_id,
+                    stop_id=stop_id,
+                    trip_id=trip_id,
+                    arrival_time=arrival_time,
+                    departure_time=departure_time
+                )
+                trip_updates.append(trip_update)
+
+        return trip_updates
 
     @staticmethod
     def should_capture_prediction(raw_arrival_time, raw_departure_time):
-      return raw_arrival_time or raw_departure_time
+        return raw_arrival_time or raw_departure_time
