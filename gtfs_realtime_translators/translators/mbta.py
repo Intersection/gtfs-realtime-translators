5ab31574397794b5983233b236c5de3e6bb79bb4
Josh Verma <45299491+joshverma@users.noreply.github.com>
2023-05-11 10:29:21 -0400

    TRAN-7078: Update MBTA translator to include static data (#52)
    * TRAN-7078: update mbta translator to use static data in addition to realtime
    
    * TRAN-7078: use default dict for dict keys
    
    * TRAN-7078: add class instantiation to types dict
    
    * TRAN-7078: add none check for scheduled relationship data
    
    * TRAN-7078: update stop name key ref
    
    * TRAN-7078: some refactoring
    
    * TRAN-7078: update mbta tests; slight refactoring
    
    * TRAN-7078: remove import stmnt
    
    * TRAN-7078: add newlines

--- gtfs_realtime_translators/translators/mbta.py
+++ gtfs_realtime_translators/translators/mbta.py
@@ -1,4 +1,5 @@
 import json
+import warnings
 
 import pendulum
 
@@ -11,7 +12,9 @@ class MbtaGtfsRealtimeTranslator:
     def __call__(self, data):
         json_data = json.loads(data)
         predictions = json_data['data']
-        entities = self.__make_trip_updates(predictions)
+        static_relationships = json_data['included']
+        static_data = self.__get_static_data(static_relationships)
+        entities = self.__make_trip_updates(predictions, static_data)
         return FeedMessage.create(entities=entities)
 
     @classmethod
@@ -19,7 +22,7 @@ class MbtaGtfsRealtimeTranslator:
         return pendulum.parse(time).in_tz(cls.TIMEZONE).int_timestamp
 
     @classmethod
-    def __make_trip_updates(cls, predictions):
+    def __make_trip_updates(cls, predictions, static_data):
         trip_updates = []
         for idx, prediction in enumerate(predictions):
             entity_id = str(idx + 1)
@@ -32,6 +35,20 @@ class MbtaGtfsRealtimeTranslator:
             raw_departure_time = attributes['departure_time']
             direction_id = attributes['direction_id']
 
+            route_color = static_data['routes'][route_id]['color']
+            route_text_color = static_data['routes'][route_id]['text_color']
+            route_long_name = static_data['routes'][route_id]['long_name']
+            route_short_name = static_data['routes'][route_id]['short_name']
+
+            headsign = static_data['trips'][trip_id]['headsign']
+
+            scheduled_arrival_time, scheduled_departure_time = \
+                cls.__get_scheduled_data(relationships['schedule'].get('data'),
+                                         static_data['schedules'])
+
+            stop_name = cls.__get_stop_data(stop_id,
+                                            static_data['stops'])
+
             if cls.__should_capture_prediction(raw_departure_time):
                 arrival_time, departure_time = cls.__set_arrival_and_departure_times(
                     raw_arrival_time, raw_departure_time)
@@ -42,11 +59,62 @@ class MbtaGtfsRealtimeTranslator:
                     trip_id=trip_id,
                     arrival_time=arrival_time,
                     departure_time=departure_time,
-                    direction_id=direction_id
+                    direction_id=direction_id,
+                    route_color=route_color,
+                    route_text_color=route_text_color,
+                    route_long_name=route_long_name,
+                    route_short_name=route_short_name,
+                    scheduled_arrival_time=scheduled_arrival_time,
+                    scheduled_departure_time=scheduled_departure_time,
+                    stop_name=stop_name,
+                    headsign=headsign,
+                    agency_timezone=cls.TIMEZONE
                 )
                 trip_updates.append(trip_update)
 
         return trip_updates
+
+    @classmethod
+    def __get_static_data(cls, static_relationships):
+        static_data = {}
+        for subclass in StaticData.__subclasses__():
+            static_data[subclass.NAME] = {}
+
+        for entity in static_relationships:
+            entity_type = entity['type']
+            static_data_type = StaticDataTypeRegistry.get(entity_type)
+            static_data[static_data_type.NAME][entity['id']] = \
+                static_data_type.create_entry(entity['id'],
+                                              entity['attributes'],
+                                              static_data_type.FIELDS)
+        return static_data
+
+    @classmethod
+    def __get_scheduled_data(cls, schedule_relationship, static_schedule_data):
+        scheduled_arrival_time, scheduled_departure_time = None, None
+        if schedule_relationship is None:
+            return scheduled_arrival_time, scheduled_departure_time
+
+        schedule_id = schedule_relationship['id']
+        schedule_data = static_schedule_data.get(schedule_id)
+
+        if schedule_data is None:
+            return scheduled_arrival_time, scheduled_departure_time
+
+        if schedule_data['arrival_time'] is not None:
+            scheduled_arrival_time = \
+                cls.__to_unix_time(schedule_data['arrival_time'])
+        if schedule_data['departure_time'] is not None:
+            scheduled_departure_time = \
+                cls.__to_unix_time(schedule_data['departure_time'])
+        return scheduled_arrival_time, scheduled_departure_time
+
+    @classmethod
+    def __get_stop_data(cls, stop_id, static_stop_data):
+        stop_data = static_stop_data.get(stop_id)
+        if stop_data is None:
+            return None
+        return stop_data['name']
 
     @classmethod
     def __set_arrival_and_departure_times(cls, raw_arrival_time, raw_departure_time):
@@ -62,3 +130,103 @@ class MbtaGtfsRealtimeTranslator:
     @classmethod
     def __should_capture_prediction(cls, raw_departure_time):
         return raw_departure_time
+
+
+class RouteShortNameTranslate:
+    ROUTE_ID_SHORT_NAMES = {
+        'Green-B': 'GL·B',
+        'Green-C': 'GL·C',
+        'Green-D': 'GL·D',
+        'Green-E': 'GL·E',
+        'Blue': 'BL',
+        'Red': 'RL',
+        'Orange': 'OL',
+        'Mattapan': 'M'
+    }
+
+    def __call__(self, entity_id, attributes_map, field):
+        if entity_id in self.ROUTE_ID_SHORT_NAMES:
+            return self.ROUTE_ID_SHORT_NAMES.get(entity_id)
+        if attributes_map['type'] == '2':
+            return attributes_map[field].replace(' Line', '')
+        return attributes_map[field]
+
+
+class IdentityTranslate:
+
+    def __call__(self, entity_id, attributes_map, field):
+        return attributes_map[field]
+
+
+class StaticData:
+    """
+    Represents a static data `type` from the MBTA API response. Implements
+    the create_entry() interface. Each instance should contain:
+        NAME: the key in the map that we create from the static data in the
+            MBTA API response
+        FIELDS: the list of fields that we want to extract from the MBTA API
+            response
+        FIELD_TRANSLATORS: optional map of fields to their translators
+    """
+    def create_entry(self, entity_id, attributes_map, fields):
+        """
+        Extracts into a map a subset of the fields from the attributes map
+        contained within each entity in the MBTA API response.
+        """
+        entry = {}
+        for field in fields:
+            field_translate = self.get_field_translator(field)
+            translated_value = field_translate(entity_id,
+                                               attributes_map,
+                                               field)
+            entry[field] = translated_value
+        return entry
+
+    def get_field_translator(self, field):
+        if not hasattr(self, 'FIELD_TRANSLATORS'):
+            return IdentityTranslate()
+        return self.FIELD_TRANSLATORS.get(field, IdentityTranslate())
+
+
+class Route(StaticData):
+    NAME = 'routes'
+    FIELDS = ['color', 'text_color', 'long_name', 'short_name']
+    FIELD_TRANSLATORS = {
+        'short_name': RouteShortNameTranslate()
+    }
+
+
+class Stop(StaticData):
+    NAME = 'stops'
+    FIELDS = ['name']
+
+
+class Schedule(StaticData):
+    NAME = 'schedules'
+    FIELDS = ['arrival_time', 'departure_time']
+
+
+class Trip(StaticData):
+    NAME = 'trips'
+    FIELDS = ['headsign']
+
+
+class StaticDataTypeRegistry:
+    """
+    This a map of the possible values in the `type` field and their
+    corresponding class representations.
+    """
+    TYPES = {'route': Route(), 'stop': Stop(), 'schedule': Schedule(),
+             'trip': Trip()}
+
+    @classmethod
+    def get(cls, key):
+        if key in cls.TYPES:
+            return cls.TYPES[key]
+        else:
+            warnings.warn(f'No static data type defined for entity type {key}',
+                          StaticDataTypeWarning)
+
+
+class StaticDataTypeWarning(Warning):
+    pass
--- test/test_mbta.py
+++ test/test_mbta.py
@@ -1,5 +1,4 @@
 import pytest
-import pendulum
 
 from gtfs_realtime_translators.translators import MbtaGtfsRealtimeTranslator
 from gtfs_realtime_translators.bindings import intersection_pb2 as intersection_gtfs_realtime
@@ -12,11 +11,20 @@ def mbta_subway():
         raw = f.read()
     return raw
 
+
+@pytest.fixture
+def mbta_subway_missing_static():
+    with open('test/fixtures/mbta_subway_missing_static.json') as f:
+        raw = f.read()
+    return raw
+
+
 @pytest.fixture
 def mbta_bus():
     with open('test/fixtures/mbta_bus.json') as f:
         raw = f.read()
     return raw
+
 
 def test_mbta_subway_realtime_arrival(mbta_subway):
     translator = MbtaGtfsRealtimeTranslator()
@@ -30,10 +38,10 @@ def test_mbta_subway_realtime_arrival(mbta_subway):
 
     assert entity.id == '1'
     assert entity.trip_update.trip.route_id == 'Blue'
-    assert entity.trip_update.trip.trip_id == '49417961'
+    assert entity.trip_update.trip.trip_id == '55458948-19:45-GovernmentCenterWonderlandSuspend'
     assert stop_time_update.stop_id == '70045'
-    assert stop_time_update.arrival.time == 1632770790
-    assert stop_time_update.departure.time == 1632770846
+    assert stop_time_update.arrival.time == 1683144315
+    assert stop_time_update.departure.time == 1683144381
 
 def test_mbta_bus_realtime_arrival_departure(mbta_bus):
     translator = MbtaGtfsRealtimeTranslator()
@@ -45,12 +53,12 @@ def test_mbta_bus_realtime_arrival_departure(mbta_bus):
 
     assert message.header.gtfs_realtime_version == FeedMessage.VERSION
 
-    assert entity.id == '2'
+    assert entity.id == '1'
     assert entity.trip_update.trip.route_id == '66'
-    assert entity.trip_update.trip.trip_id == '49181421'
+    assert entity.trip_update.trip.trip_id == '55800441'
     assert stop_time_update.stop_id == '1357'
-    assert stop_time_update.arrival.time == 1632778733
-    assert stop_time_update.departure.time == 1632778733
+    assert stop_time_update.arrival.time == 1683145580
+    assert stop_time_update.departure.time == 1683145580
 
 def test_mbta_bus_realtime_no_arrival_departure(mbta_bus):
     translator = MbtaGtfsRealtimeTranslator()
@@ -62,12 +70,12 @@ def test_mbta_bus_realtime_no_arrival_departure(mbta_bus):
 
     assert message.header.gtfs_realtime_version == FeedMessage.VERSION
 
-    assert entity.id == '3'
+    assert entity.id == '2'
     assert entity.trip_update.trip.route_id == '66'
-    assert entity.trip_update.trip.trip_id == '49181349'
+    assert entity.trip_update.trip.trip_id == '55800443'
     assert stop_time_update.stop_id == '1357'
-    assert stop_time_update.arrival.time == 1632778733
-    assert stop_time_update.departure.time == 1632778733
+    assert stop_time_update.arrival.time == 1683145945
+    assert stop_time_update.departure.time == 1683145945
 
 def test_mbta_bus_realtime_arrival_no_departure(mbta_bus):
     translator = MbtaGtfsRealtimeTranslator()
@@ -79,9 +87,59 @@ def test_mbta_bus_realtime_arrival_no_departure(mbta_bus):
 
     assert message.header.gtfs_realtime_version == FeedMessage.VERSION
 
-    assert entity.id == '5'
+    assert entity.id == '3'
     assert entity.trip_update.trip.route_id == '66'
-    assert entity.trip_update.trip.trip_id == '49181351'
+    assert entity.trip_update.trip.trip_id == '55800446'
     assert stop_time_update.stop_id == '1357'
-    assert stop_time_update.arrival.time == 1632780015
-    assert stop_time_update.departure.time == 1632780015
+    assert stop_time_update.arrival.time == 1683146412
+    assert stop_time_update.departure.time == 1683146412
+
+def test_mbta_subway_realtime_include_static_data(mbta_subway):
+    translator = MbtaGtfsRealtimeTranslator()
+    message = translator(mbta_subway)
+
+    entity = message.entity[0]
+    trip_update = entity.trip_update
+    stop_time_update = trip_update.stop_time_update[0]
+
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_short_name == 'BL'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_long_name == 'Blue Line'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_color == '003DA5'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_text_color == 'FFFFFF'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].headsign == 'Bowdoin'
+    assert stop_time_update.Extensions[
+        intersection_gtfs_realtime.intersection_stop_time_update].stop_name == 'Maverick'
+    assert stop_time_update.Extensions[
+        intersection_gtfs_realtime.intersection_stop_time_update].scheduled_arrival.time == 1683297180
+    assert stop_time_update.Extensions[
+        intersection_gtfs_realtime.intersection_stop_time_update].scheduled_departure.time == 1683297180
+
+def test_mbta_subway_realtime_missing_static_data(mbta_subway_missing_static):
+    translator = MbtaGtfsRealtimeTranslator()
+    message = translator(mbta_subway_missing_static)
+
+    entity = message.entity[0]
+    trip_update = entity.trip_update
+    stop_time_update = trip_update.stop_time_update[0]
+
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_short_name == 'BL'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_long_name == 'Blue Line'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_color == '003DA5'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].route_text_color == 'FFFFFF'
+    assert trip_update.Extensions[
+        intersection_gtfs_realtime.intersection_trip_update].headsign == 'Bowdoin'
+    assert stop_time_update.Extensions[
+        intersection_gtfs_realtime.intersection_stop_time_update].stop_name == ''
+    assert stop_time_update.Extensions[
+        intersection_gtfs_realtime.intersection_stop_time_update].scheduled_arrival.time == 0
+    assert stop_time_update.Extensions[
+        intersection_gtfs_realtime.intersection_stop_time_update].scheduled_departure.time == 0
