1046aed80224171d51da844bc73a268dffc2e40b
Josh Verma <josh.verma@intersection.com>
2025-07-29 17:56:21 -0400

    TRAN-8908: function

--- gtfs_realtime_translators/translators/njt_bus_json.py
+++ gtfs_realtime_translators/translators/njt_bus_json.py
@@ -23,30 +23,6 @@ class NjtBusJsonGtfsRealtimeTranslator:
         return FeedMessage.create(entities=entities)
 
     @classmethod
-    def __parse_time_to_unix(cls, time_str):
-        """
-        Parse time string to Unix timestamp.
-        Handles formats like "01:40 PM" and "7/25/2025 1:40:00 PM"
-        """
-        try:
-            # Try parsing full datetime format first
-            if '/' in time_str:
-                dt = datetime.strptime(time_str, '%m/%d/%Y %I:%M:%S %p')
-                # Convert to pendulum with timezone
-                pdt = pendulum.instance(dt, tz=cls.TIMEZONE)
-                return int(pdt.timestamp())
-            else:
-                # For time-only format, use current date
-                current_date = pendulum.now(cls.TIMEZONE).format('YYYY-MM-DD')
-                full_datetime_str = f"{current_date} {time_str}"
-                dt = datetime.strptime(full_datetime_str, '%Y-%m-%d %I:%M %p')
-                pdt = pendulum.instance(dt, tz=cls.TIMEZONE)
-                return int(pdt.timestamp())
-        except ValueError:
-            # If parsing fails, return current timestamp
-            return int(pendulum.now(cls.TIMEZONE).timestamp())
-
-    @classmethod
     def __make_trip_updates(cls, items, stop_id):
         trip_updates = []
         
@@ -58,8 +34,8 @@ class NjtBusJsonGtfsRealtimeTranslator:
             departure_time_str = item.get('departuretime', '')
             scheduled_departure_str = item.get('sched_dep_time', '')
             
-            departure_time = cls.__parse_time_to_unix(departure_time_str)
-            scheduled_departure_time = cls.__parse_time_to_unix(scheduled_departure_str)
+            departure_time = cls.__time_to_unix_time(departure_time_str)
+            scheduled_departure_time = cls.__time_to_unix_time(scheduled_departure_str)
             
             arrival_time = departure_time
             scheduled_arrival_time = scheduled_departure_time
@@ -83,3 +59,24 @@ class NjtBusJsonGtfsRealtimeTranslator:
 
         return trip_updates
 
+    @classmethod
+    def __time_to_unix_time(cls, time_str):
+        """
+        Tries to parse a datetime string as either:
+        - 'M/D/YYYY h:mm:ss A'
+        - 'h:mm A' (uses today's date)
+
+        Returns: Unix timestamp (int)
+        """
+        time_str = time_str.strip()
+
+        try:
+            dt = pendulum.from_format(time_str, "M/D/YYYY h:mm:ss A",
+                                      tz=cls.TIMEZONE)
+        except pendulum.parsing.exceptions.ParserError:
+            today = pendulum.today(tz=cls.TIMEZONE).to_date_string()
+            dt = pendulum.from_format(f"{today} {time_str}",
+                                      "YYYY-MM-DD h:mm A",
+                                      tz=cls.TIMEZONE)
+
+        return dt.int_timestamp
